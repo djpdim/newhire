84a3097ebace33d257272575e07866bba783c87c
Dimosthenis Pagkas <dimospagkas@yahoo.gr>
2024-07-26 10:18:45 -0400

    Fix capitalization for the other access needed section

--- src/components/OtherAccessSection.js
+++ src/components/OtherAccessSection.js
@@ -3,12 +3,18 @@
 import React from "react";
 
 const OtherAccessSection = ({ formData, handleChange }) => {
+    const handleInputChange = event => {
+        const { name, value } = event.target;
+        const formattedValue = value.replace(/(?:^|\. )\w/g, char => char.toUpperCase());
+        handleChange({ target: { name, value: formattedValue } });
+    };
+
     return (
         <>
             <h3>Other Access Requirements</h3>
             <label>
                 Other Access Needed:
-                <textarea name='otherAccess' value={formData.otherAccess} onChange={handleChange} />
+                <textarea name='otherAccess' value={formData.otherAccess} onChange={handleInputChange} />
             </label>
         </>
     );
