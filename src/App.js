772bca2a75002c7bfeeed4674ebc9b9a048e4259
Dimosthenis Pagkas <dimospagkas@yahoo.gr>
2024-07-26 11:04:59 -0400

    Fixed refreshing effect by scrolling to the top and update some comments

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 /** @format */
 
 // App.js
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
-// import CompanyLogo from "./assets/megalg.svg"; // Import the company logo image
 import NewHireForm from "./NewHireForm";
 
 function App() {
+    useEffect(() => {
+        // scroll to top on refresh
+        window.onbeforeunload = () => {
+            window.scrollTo(0, 0);
+        };
+    }, []);
+
     return (
         <div className='App NewHireForm'>
-            {/* <header className='App-header'>
-                <img src={CompanyLogo} alt='Company Logo' className='logo' />
-                <h1 className='text-xl font-bold'>New Hire IT Checklist Form</h1>
-            </header> */}
             <main>
                 <NewHireForm />
             </main>
