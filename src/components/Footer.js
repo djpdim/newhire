b42d9925bacafab6713c80f7153c3d9f5d03dc7e
Dimosthenis Pagkas <dimospagkas@yahoo.gr>
2024-07-29 14:39:02 -0400

    update css to match mobile version

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ import CompanyLogo from "../assets/megalg.svg"; // Adjust path if necessary
 const Footer = () => {
     return (
         <div className='footer'>
-            <img src={CompanyLogo} alt='Company Logo' className='logo' />
+            <img src={CompanyLogo} alt='Company Logo' className='footerlogo' />
             <p>&copy; 2024 Mega Contracting Group. All rights reserved.</p>
         </div>
     );
